80f3200c18a56d60b42a8386a5f7a9fafa84baaa
IraMira333 <irinat333@gmail.com>
2025-08-31 02:42:41 +0300

    Test commit

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,13 +2,7 @@ export default function Home() {
   return (
     <div className=" grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row"></div>
+        Save and see your changes instantly.
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
