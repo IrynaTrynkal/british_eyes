ab99cbbffd9740a63cb630ab5a140af336cd0f1b
IraMira333 <irinat333@gmail.com>
2025-09-01 00:07:04 +0300

    sorted

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,10 +3,10 @@ import { defineRouting } from "next-intl/routing";
 export const locales = ["uk", "en", "ru"] as const;
 
 export const routing = defineRouting({
-  // A list of all locales that are supported
-  locales,
-  localePrefix: "as-needed",
-  localeDetection: false,
-  // Used when no locale matches
-  defaultLocale: "uk",
+    // A list of all locales that are supported
+    locales,
+    localePrefix: "as-needed",
+    localeDetection: false,
+    // Used when no locale matches
+    defaultLocale: "uk",
 });
