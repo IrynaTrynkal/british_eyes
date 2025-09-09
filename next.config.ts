48c4e2829511f84d490b2d5547be79190a71b9ee
IraMira333 <irinat333@gmail.com>
2025-09-09 23:59:25 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    experimental: {
+        serverActions: { bodySizeLimit: "2mb" },
+    },
+    reactStrictMode: true,
+    productionBrowserSourceMaps: true,
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
