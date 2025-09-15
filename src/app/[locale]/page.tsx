f5d3cc31384594ac7135768183359a8c563b9981
IraMira333 <irinat333@gmail.com>
2025-09-15 21:37:48 +0300

    Created Feedbacks

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,6 +3,7 @@ import { Advantages } from "@/components/main/advantages/Advantages";
 import { Approach } from "@/components/main/approach/Approach";
 import { Choice } from "@/components/main/choice/Choice";
 import { Doctors } from "@/components/main/doctors/Doctors";
+import { Feedbacks } from "@/components/main/feedbacks/Feedbacks";
 import { HeroMain } from "@/components/main/hero/HeroMain";
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
             <Approach />
             <AboutMain />
             <Doctors />
+            <Feedbacks />
         </>
     );
 }
