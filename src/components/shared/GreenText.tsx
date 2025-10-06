1b8cd9c769180096ed7bed07a48b5d6e1b2050bc
IraMira333 <irinat333@gmail.com>
2025-10-06 14:32:39 +0300

    Added German Section

--- src/components/shared/GreenText.tsx
+++ src/components/shared/GreenText.tsx
@@ -1,13 +1,15 @@
 export const GreenText = ({
     text,
+    grey,
     className,
 }: {
     text: string | React.ReactNode;
+    grey?: boolean;
     className?: string;
 }) => {
     return (
         <div
-            className={`text-emerald font-oswald border-emerald border-l pl-2 leading-5 ${className}`}
+            className={`text-emerald font-oswald ${grey ? "border-grey" : "border-emerald"} border-emerald border-l pl-2 leading-5 ${className}`}
         >
             {text}
         </div>
