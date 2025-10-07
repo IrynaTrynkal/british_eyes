a31603e46a640d01fc8a915f81749b860330045c
IraMira333 <irinat333@gmail.com>
2025-10-07 18:59:04 +0300

    Added Methods Section and new subpages

---
+++ src/components/pageAbout/methods/Methods.tsx
@@ -0,0 +1,38 @@
+import { useTranslations } from "next-intl";
+
+import { methodsList } from "@/components/assets/aboutData";
+import { GreenText } from "@/components/shared/GreenText";
+
+import { MethodCard } from "./MethodCard";
+
+export const Methods = () => {
+    const t = useTranslations("AboutPage");
+
+    return (
+        <section className="content relative pb-[60px]">
+            <div className="prepc:mb-0 mb-6 flex w-full justify-between">
+                <h3 className="subtitle prepc:absolute prepc:top-0 prepc:left-6 pc:left-12">
+                    {t("methodsSubtitle")}
+                </h3>
+                <GreenText
+                    grey
+                    text={t("methodsGreen")}
+                    className="prepc:absolute prepc:top-0 prepc:right-6 pc:right-12 w-[202px]"
+                />
+            </div>
+            <div className="tab:max-w-[500px] prepc:mb-10 pc:max-w-[750px] tab:text-center tab:mx-auto mb-5">
+                <h2 className="title-section prepc:mb-7 tab:max-w-[560px] prepc:max-w-full mb-6 max-w-[440px]">
+                    {t("methodsTitle")}
+                </h2>
+                <p className="pc:text-lg pc:mx-auto pc:max-w-[611px] leading-5">
+                    {t("methodsText")}
+                </p>
+            </div>
+            <div className="tab:flex-row tab:gap-3 prepc:gap-5 flex flex-col gap-2">
+                {methodsList.map(item => (
+                    <MethodCard key={item.data.en.title} data={item} />
+                ))}
+            </div>
+        </section>
+    );
+};
