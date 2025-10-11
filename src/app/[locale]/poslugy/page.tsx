c4fb003d459b36a5ce4d271b1a4be0a6ffb373c0
IraMira333 <irinat333@gmail.com>
2025-10-11 12:40:04 +0300

    Added Hero to all services page

--- src/app/[locale]/poslugy/page.tsx
+++ src/app/[locale]/poslugy/page.tsx
@@ -1,5 +1,6 @@
 import { Doctors } from "@/components/main/doctors/Doctors";
 import { Feedbacks } from "@/components/main/feedbacks/Feedbacks";
+import { HeroServices } from "@/components/pageServices/HeroServices/HeroServices";
 import { Booking } from "@/components/shared/booking/Booking";
 import { Breadcrumbs } from "@/components/shared/Breadcrumbs";
 
@@ -8,6 +9,7 @@ export default function ServicesPage() {
 
     return (
         <>
+            <HeroServices />
             <div className="content py-24">
                 <Breadcrumbs breadcrumbsList={breadcrumb} />
             </div>
