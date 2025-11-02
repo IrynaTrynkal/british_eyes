ae5b2eccc537572c9584b6e82d23e4aafac2fb9d
IraMira333 <irinat333@gmail.com>
2025-11-02 22:54:21 +0100

    Added hero to instruction pages

--- src/app/[locale]/informatsiya-dlya-inohorodnikh-patsiyentiv/page.tsx
+++ src/app/[locale]/informatsiya-dlya-inohorodnikh-patsiyentiv/page.tsx
@@ -1,5 +1,12 @@
+import { notFound } from "next/navigation";
+import { useLocale } from "next-intl";
+
+import { pationtsInstructionsData } from "@/components/assets/patientsInstructionData";
 import { Breadcrumbs } from "@/components/shared/Breadcrumbs";
 import { Global } from "@/components/shared/global/Global";
+import { TextTypeRender } from "@/components/shared/TextTypeRender";
+import { HeroInstruction } from "@/components/someInstructionComponents/HeroInstruction";
+import { LocaleType } from "@/types/LocaleType";
 
 export default function PatientsNonresidentPage() {
     const breadcrumb = [
@@ -8,13 +15,26 @@ export default function PatientsNonresidentPage() {
             href: "/informatsiya-dlya-inohorodnikh-patsiyentiv",
         },
     ];
+    const locale = useLocale();
+    const data = pationtsInstructionsData.find(
+        instr => instr.name.key === "informatsiya-dlya-inohorodnikh-patsiyentiv"
+    );
+    if (!data) return notFound();
+
     return (
         <>
             <Breadcrumbs
                 breadcrumbsList={breadcrumb}
                 className="prepc:mt-[104px] prepc:mb-12 mt-[72px] mb-6"
             />
-
+            <HeroInstruction title={data[locale as LocaleType].title} />
+            <section className="tab:pb-12 tab:px-6 pc:px-12 pc:pb-[120px] pb-[60px]">
+                <div className="bg-green-10 tab:mx-0 tab:max-w-[1117px] prepc:w-[83%] prepc:p-6 prepc:rounded-lg mx-auto max-w-[540px] rounded p-4">
+                    {data[locale as LocaleType].content.map((item, ind) => (
+                        <TextTypeRender key={ind} data={item.text} />
+                    ))}
+                </div>
+            </section>
             <Global />
         </>
     );
--- src/app/[locale]/preparation-for-surgery-for-strabismus/page.tsx
+++ src/app/[locale]/preparation-for-surgery-for-strabismus/page.tsx
@@ -1,4 +1,10 @@
+import { notFound } from "next/navigation";
+import { useLocale } from "next-intl";
+
+import { pationtsInstructionsData } from "@/components/assets/patientsInstructionData";
 import { Breadcrumbs } from "@/components/shared/Breadcrumbs";
+import { HeroInstruction } from "@/components/someInstructionComponents/HeroInstruction";
+import { LocaleType } from "@/types/LocaleType";
 
 export default function PatientsStrabismusInstructionPage() {
     const breadcrumb = [
@@ -7,13 +13,19 @@ export default function PatientsStrabismusInstructionPage() {
             href: "/preparation-for-surgery-for-strabismus",
         },
     ];
+    const locale = useLocale();
+    const data = pationtsInstructionsData.find(
+        instr => instr.name.key === "preparation-for-surgery-for-strabismus"
+    );
+    if (!data) return notFound();
+
     return (
         <>
             <Breadcrumbs
                 breadcrumbsList={breadcrumb}
                 className="prepc:mt-[104px] prepc:mb-12 mt-[72px] mb-6"
             />
-            <p> підготовка до операціїї з приводу косоокості</p>
+            <HeroInstruction title={data[locale as LocaleType].title} />
         </>
     );
 }
