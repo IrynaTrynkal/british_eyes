355b87c530d62aae9587605a78441ca00d27b792
IraMira333 <irinat333@gmail.com>
2025-11-12 22:39:06 +0100

    Reduce green sections

--- src/components/main/approach/Approach.tsx
+++ src/components/main/approach/Approach.tsx
@@ -7,9 +7,9 @@ export const Approach = () => {
     const t = useTranslations("HomePage");
     const locale = useLocale();
     return (
-        <section className="text-ivory tab:py-12 pc:py-[120px] bg-[url(/images/approach.jpg)] bg-cover bg-center bg-no-repeat py-6">
+        <section className="text-ivory tab:py-12 bg-[url(/images/approach.jpg)] bg-cover bg-center bg-no-repeat py-6">
             <div className="content">
-                <h2 className="title-section-ivory tab:max-w-[560px] tab:mb-40 mb-[100px] max-w-[440px]">
+                <h2 className="title-section-ivory tab:max-w-[560px] tab:mb-20 max-w-[440px]">
                     {t("approachTitle")}
                 </h2>
                 <div className="mr-0 ml-auto max-w-[777px]">
