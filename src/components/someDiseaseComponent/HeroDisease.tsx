5a7623b54b18923b42bf894ef8f3b3179aa7ac54
IraMira333 <irinat333@gmail.com>
2025-11-12 23:03:17 +0100

    Reduced the height of all heroes in the mobile version

--- src/components/someDiseaseComponent/HeroDisease.tsx
+++ src/components/someDiseaseComponent/HeroDisease.tsx
@@ -1,6 +1,6 @@
 export const HeroDisease = ({ title }: { title: string }) => {
     return (
-        <section className="tab:mt-0 cta-green-gradient tab:h-120 prepc:h-[552px] relative mt-12 h-[calc(100vh-48px)] overflow-hidden">
+        <section className="tab:mt-0 cta-green-gradient tab:h-80 relative mt-12 h-[calc(50vh-48px)] overflow-hidden">
             <div className="content flex h-full items-end">
                 <h1 className="title-h1-up text-white-100 prepc:max-w-[553px] tab:mb-5 prepc:mb-12 mt-auto mb-10 max-w-[400px] whitespace-pre-line uppercase">
                     {title}
