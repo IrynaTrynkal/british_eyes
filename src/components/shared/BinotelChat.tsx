3aed47eb2a5edf563e8126cd3ce6aa94256d9d4c
IraMira333 <irinat333@gmail.com>
2025-11-24 22:40:06 +0200

    Update BinotelChat.tsx

--- src/components/shared/BinotelChat.tsx
+++ src/components/shared/BinotelChat.tsx
@@ -1,59 +1,69 @@
-"use client";
-
 import Script from "next/script";
 
-export const BinotelChat = () => {
+export function BinotelChat() {
     return (
         <>
-            {/* <Script id="binotel-chat" strategy="afterInteractive">
+            {/* === Мультимовність для GetCall === */}
+            <Script id="binotel-getcall-lang" strategy="afterInteractive">
                 {`
-                (function(d, w, s) {
-                  var widgetHash = 'nYlU4dOMPLM1vnvhEoRB',
-                      bch = d.createElement(s);
-                  bch.type = 'text/javascript';
-                  bch.async = true;
-                  bch.src = '//widgets.binotel.com/chat/widgets/' + widgetHash + '.js';
-                  var sn = d.getElementsByTagName(s)[0];
-                  sn.parentNode.insertBefore(bch, sn);
-                })(document, window, 'script');
-                `}
-            </Script>
-
-            <Script id="binotel-callback" strategy="afterInteractive">
-                {`
-                (function(d, w, s) {
-                  var widgetHash = 'rz5porl99lpafaaatagd',
-                      gcw = d.createElement(s);
-                  gcw.type = 'text/javascript';
-                  gcw.async = true;
-                  gcw.src = '//widgets.binotel.com/getcall/widgets/' + widgetHash + '.js';
-                  var sn = d.getElementsByTagName(s)[0];
-                  sn.parentNode.insertBefore(gcw, sn);
-                })(document, window, 'script');
-                `}
-            </Script> */}
-            <Script id="binotel-lang" strategy="afterInteractive">
-                {`
-  (function() {
+(function() {
     var path = window.location.pathname.toLowerCase();
     var lang = 'ua';
 
-    if (path.indexOf('/ru') === 0) {
-      lang = 'ru';
-    } else if (path.indexOf('/en') === 0) {
-      lang = 'en';
-    }
+    if (path.startsWith('/ru')) lang = 'ru';
+    else if (path.startsWith('/en')) lang = 'en';
+    else if (path.startsWith('/ua')) lang = 'ua';
 
     window.BinotelGetCallSettings = { language: lang };
-  })();
-`}
+})();
+                `}
             </Script>
 
-            <Script
-                id="binotel-widget"
-                strategy="afterInteractive"
-                src="//widgets.binotel.com/getcall/widgets/rz5porl99lpafaaatagd.js"
-            />
+            {/* === GetCall Widget === */}
+            <Script id="binotel-getcall" strategy="afterInteractive">
+                {`
+(function(d, w, s) {
+    var widgetHash = 'rz5porl99lpafaaatagd';
+    var gcw = d.createElement(s);
+    gcw.type = 'text/javascript';
+    gcw.async = true;
+    gcw.src = '//widgets.binotel.com/getcall/widgets/' + widgetHash + '.js';
+    var sn = d.getElementsByTagName(s)[0];
+    sn.parentNode.insertBefore(gcw, sn);
+})(document, window, 'script');
+                `}
+            </Script>
+
+            {/* === Мультимовність для Chat === */}
+            <Script id="binotel-chat-lang" strategy="afterInteractive">
+                {`
+(function() {
+    var path = window.location.pathname.toLowerCase();
+    var lang = 'ua';
+
+    if (path.startsWith('/ru')) lang = 'ru';
+    else if (path.startsWith('/en')) lang = 'en';
+    else if (path.startsWith('/ua')) lang = 'ua';
+
+    window.BinotelChatSettings = { language: lang };
+})();
+                `}
+            </Script>
+
+            {/* === Chat Widget === */}
+            <Script id="binotel-chat" strategy="afterInteractive">
+                {`
+(function(d, w, s) {
+    var widgetHash = 'nYlU4dOMPLM1vnvhEoRB';
+    var bch = d.createElement(s);
+    bch.type = 'text/javascript';
+    bch.async = true;
+    bch.src = '//widgets.binotel.com/chat/widgets/' + widgetHash + '.js';
+    var sn = d.getElementsByTagName(s)[0];
+    sn.parentNode.insertBefore(bch, sn);
+})(document, window, 'script');
+                `}
+            </Script>
         </>
     );
-};
+}
