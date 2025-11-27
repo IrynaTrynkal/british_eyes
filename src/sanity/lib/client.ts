859079e1be28c084e8b92ce22aacd97ac318f116
IraMira333 <irinat333@gmail.com>
2025-11-27 23:32:26 +0200

    Changed Sanity

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -6,24 +6,26 @@ export const client = createClient({
     projectId,
     dataset,
     apiVersion,
-    useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+    useCdn: true,
+    perspective: "published",
 });
 
 export async function sanityFetch<const QueryString extends string>({
     query,
     params = {},
-    // revalidate = 60, // default revalidation time in seconds
     tags = [],
+    revalidate = 3600,
 }: {
     query: QueryString;
     params?: QueryParams;
-    // revalidate?: number | false;
     tags?: string[];
+    revalidate?: number | false;
 }) {
     return client.fetch(query, params, {
+        cache: "force-cache",
         next: {
-            // revalidate: tags.length ? false : revalidate, // for simple, time-based revalidation
-            tags, // for tag-based revalidation
+            revalidate: tags.length ? false : revalidate,
+            tags,
         },
     });
 }
